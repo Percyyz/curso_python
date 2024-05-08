971e5ac69e5dfdda3760fbff0d69e480c960e927
percyz30 <percynoeyarihuaman@gmail.com>
2024-05-08 08:29:58 -0500

    hoy cree mi ejercicio uno

---
+++ CONTROLES_DE_FLUJO/ejercicios.py
@@ -0,0 +1,8 @@
+## escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor
+#de edad ono.
+edad_usuario=int(input("ingrese su edad :"))
+if edad_usuario>=18:
+    print("es mayor de edad")
+else:
+    print("no es mayor de edad")
+
