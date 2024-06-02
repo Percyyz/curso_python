85c037eeb276158f686575a6170caebee776e3ad
percyz30 <percynoeyarihuaman@gmail.com>
2024-06-02 08:45:15 -0500

    guardo la tarea realizada sobre: lista de mascotas

--- TIPO_DE_DATOS_ESTRUCTURADOS/homework.py
+++ TIPO_DE_DATOS_ESTRUCTURADOS/homework.py
@@ -45,3 +45,39 @@ indice=lista1.index({
    "edad":11 
 })
 print(lista1[indice])
+
+# 2 CREAR UNA LISTA CON 4 DICCIONARIOS DONDE TENTENDRAN LOS DATOS DE SUS MASCOTAS (NOMBRE, EDAD, SEXO Y RAZA)
+## TAREAS
+# 1. MOSTRAR LA LISTA CON LOS 4 DICCIONARIOS
+# 2. EDITAR EL TERCER REGISTRO Y CAMBIARLE LA EDAD SIN MODIFICAR LA LISTA ORIGINAL
+# 3. MOSTRAR LA LISTA ORIGINAL Y LUEGO LA LISTA CON EL TERCER REGISTRO MODIFICADO
+lista_mascotas=[{
+    "nombre":"Toby",
+    "edad":3,
+    "sexo":"Macho",
+    "raza":"Pastor Aleman"
+},
+{
+    "nombre":"Luna",
+    "edad":2,
+    "sexo":"Hembra",
+    "raza":"Chihuahua"
+},
+{
+    "nombre":"Pancho",
+    "edad":1,
+    "sexo":"Macho",
+    "raza":"Dalmata"
+},
+{
+    "nombres":"bella",
+    "edad":4,
+    "sexo":"hembra",
+    "raza":"Boxer"
+}]
+print(lista_mascotas)
+
+copia_lista=lista_mascotas.copy()
+print(lista_mascotas)
+copia_lista[3]["edad"]=2
+print(copia_lista)
