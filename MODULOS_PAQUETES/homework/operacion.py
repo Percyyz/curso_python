9721c7891ae469e39afb7364147a6843cef5afb3
percyz30 <percynoeyarihuaman@gmail.com>
2024-07-12 15:23:35 -0500

    guardo la tarea de importacion de 5 variables y una funcion en modelo

---
+++ MODULOS_PAQUETES/homework/operacion.py
@@ -0,0 +1,11 @@
+# operaciones
+
+variable1:int = 100
+variable2:int = 200
+variable3:int = 300
+variable4:int = 400
+variable5:int = 500
+
+def mi_funcion():
+    """Esta función retorna un saludo"""
+    return "Hola desde mi_funcion"
---
+++ MODULOS_PAQUETES/homework/tarea.py
@@ -0,0 +1,20 @@
+# importacion directa
+import operacion
+
+var1:int=operacion.variable1
+print(var1)
+
+var2:int=operacion.variable2
+print(var2)
+
+var3:int=operacion.variable3
+print(var3)
+
+var4:int=operacion.variable4
+print(var4)
+
+var5:int=operacion.variable5
+print(var5)
+
+saludo:str=operacion.mi_funcion()
+print(saludo)
