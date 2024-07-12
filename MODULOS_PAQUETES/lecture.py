ab616d066014a69b18d5a1f613484d266a4a3739
percyz30 <percynoeyarihuaman@gmail.com>
2024-07-12 10:26:09 -0500

    guardo la clase de modulos

---
+++ MODULOS_PAQUETES/lecture.py
@@ -0,0 +1,22 @@
+# # practicando pylint
+# nombre_alumno:str="miguel"
+
+# def suma(a:int,b:int):
+#     """"funcion para sumar dos num eros"""
+#     return a+b
+# print(suma(4,7))
+
+
+# def resta(a:int,b:int):
+#     """"funcion para restar dos num eros"""
+#     return a-b
+# print(resta(45,23))
+
+
+# importacion directa
+import operaciones
+suma:int=operaciones.suma(7,8)
+print(suma)
+
+msj:str=f"{operaciones.mensaje}, como estas"
+print(msj)
---
+++ MODULOS_PAQUETES/operaciones.py
@@ -0,0 +1,14 @@
+""""modulo de operaciones aritmeticas basicas"""
+def suma(a,b):
+    """"funcion de suma"""
+    return a+b
+
+def resta(a,b):
+    """"funcion de suma"""
+    return a-b
+
+def producto(a,b):
+    """"funcion de suma"""
+    return a*b
+
+mensaje:str="hola"
